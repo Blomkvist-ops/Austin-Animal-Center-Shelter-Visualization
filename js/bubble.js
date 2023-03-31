752e6332fe11d6efc1c1da4705c4800b01188cbb
施恩光 <samuel@EnguangShi-MacBook-Pro.local>
2023-03-30 18:10:04 -0700

    Fixed the bubble chart and the axis line

--- js/bubble.js
+++ js/bubble.js
@@ -1,183 +1,227 @@
 class BubbleChart {
+  /**
+   * Class constructor with initial configuration
+   * @param {Object}
+   * @param {Array}
+   */
+  constructor(_config, _data) {
+    this.config = {
+      parentElement: _config.parentElement,
+      containerWidth: 600,
+      containerHeight: 800,
+      margin: { top: 15, right: 10, bottom: 70, left: 60 },
+      tooltipPadding: 15,
+    };
+    this.data = _data;
+    this.selectedCategories = [];
+    this.initVis();
+  }
 
-    /**
-     * Class constructor with initial configuration
-     * @param {Object}
-     * @param {Array}
-     */
-    constructor(_config, _data) {
-        this.config = {
-            parentElement: _config.parentElement,
-            containerWidth: 600,
-            containerHeight: 800,
-            margin: { top: 15, right: 10, bottom: 70, left: 60 },
-            tooltipPadding: 15,
-        }
-        this.data = _data;
-        this.selectedCategories = [];
-        this.initVis();
-    }
+  /**
+   * Create scales, axes, and append static elements
+   */
+  initVis() {
+    let vis = this;
 
-    /**
-     * Create scales, axes, and append static elements
-     */
-    initVis() {
-        let vis = this;
+    // Define size of SVG drawing area
+    vis.svg = d3
+      .select(vis.config.parentElement)
+      .attr("width", vis.config.containerWidth)
+      .attr("height", vis.config.containerHeight);
 
-        // Define size of SVG drawing area
-        vis.svg = d3.select(vis.config.parentElement)
-            .attr('width', vis.config.containerWidth)
-            .attr('height', vis.config.containerHeight);
+    vis.width =
+      vis.config.containerWidth -
+      vis.config.margin.left -
+      vis.config.margin.right;
+    vis.height =
+      vis.config.containerHeight -
+      vis.config.margin.top -
+      vis.config.margin.bottom;
 
-        vis.width = vis.config.containerWidth - vis.config.margin.left - vis.config.margin.right;
-        vis.height = vis.config.containerHeight - vis.config.margin.top - vis.config.margin.bottom;
+    // Append group element that will contain our actual chart
+    // and position it according to the given margin config
+    vis.chartArea = vis.svg
+      .append("g")
+      .attr(
+        "transform",
+        `translate(${vis.config.margin.left},${vis.config.margin.top})`
+      );
 
-        // Append group element that will contain our actual chart 
-        // and position it according to the given margin config
-        vis.chartArea = vis.svg.append('g')
-            .attr('transform', `translate(${vis.config.margin.left},${vis.config.margin.top})`);
+    vis.chart = vis.chartArea.append("g");
 
-        vis.chart = vis.chartArea.append('g');
+    vis.svg
+      .append("text")
+      .attr("class", ".title")
+      .attr("x", 0)
+      .attr("y", 100)
+      .attr("dy", ".71em")
+      .text("Breed Distribution");
 
-        vis.svg.append('text')
-            .attr('class', '.title')
-            .attr('x', 0)
-            .attr('y', 100)
-            .attr('dy', '.71em')
-            .text('Breed Distribution');
+    // Color palette for animal types
+    vis.color = d3
+      .scaleOrdinal()
+      .domain(["Dog", "Cat", "Bird", "Other"])
+      .range(d3.schemeTableau10);
 
-        // Color palette for animal types
-        vis.color = d3.scaleOrdinal()
-            .domain(["Dog", "Cat", "Bird", "Other"])
-            .range(d3.schemeTableau10);
+    // Size scale for countries
+    vis.size = d3.scaleLinear().range([15, 80]); // circle will be between 7 and 55 px wide
 
-        // Size scale for countries
-        vis.size = d3.scaleLinear()
-            .range([15, 80])  // circle will be between 7 and 55 px wide
+    // Features of the forces applied to the nodes:
+    vis.simulation = d3
+      .forceSimulation()
+      .force(
+        "charge",
+        d3.forceManyBody().strength(function (d) {
+          return Math.pow(d.radius, 2.0) * 0.03;
+        })
+      )
+      .force(
+        "x",
+        d3
+          .forceX()
+          .strength(0.03)
+          .x(vis.width / 2)
+      )
+      .force(
+        "y",
+        d3
+          .forceY()
+          .strength(0.03)
+          .y(vis.height / 2)
+      )
+      .force(
+        "collision",
+        d3.forceCollide().radius((d) => d.radius + 2)
+      )
+      .alphaTarget(0.1)
+      .alphaDecay(0.01);
+  }
 
+  updateVis() {
+    let vis = this;
 
-        // Features of the forces applied to the nodes:
-        vis.simulation = d3.forceSimulation()
-            .force('charge', d3.forceManyBody().strength(function (d) {
-                return Math.pow(d.radius, 2.0) * 0.03
-            }))
-            .force('x', d3.forceX().strength(0.03).x(vis.width / 2))
-            .force('y', d3.forceY().strength(0.03).y(vis.height / 2))
-            .force('collision', d3.forceCollide().radius(d => d.radius + 2));
+    vis.group = Array.from(
+      d3.rollup(
+        vis.data,
+        (v) => v.length,
+        (d) => d.breed,
+        (d) => d.animal_type
+      ),
+      ([breed, type, value]) => ({
+        breed: breed,
+        type: Array.from(type.keys())[0],
+        value: Array.from(type.values())[0],
+      })
+    )
+      .sort((a, b) => b.value - a.value)
+      .slice(0, 100);
 
-    }
+    vis.size.domain(d3.extent(vis.group, (d) => d.value));
 
-    updateVis() {
-        let vis = this;
+    vis.nodes = vis.group.map((d) => ({
+      ...d,
+      radius: vis.size(d.value),
+      x: Math.random() * 900,
+      y: Math.random() * 800,
+    }));
 
-        vis.group = Array.from(d3.rollup(vis.data, v => v.length, d => d.breed, d => d.animal_type), ([breed, type, value]) => ({
-            breed: breed,
-            type: Array.from(type.keys())[0],
-            value: Array.from(type.values())[0]
-        })).sort((a, b) => b.value - a.value).slice(0, 100);
+    vis.renderVis();
+  }
 
-        vis.size.domain(d3.extent(vis.group, d => d.value));
+  renderVis() {
+    let vis = this;
 
-        vis.nodes = vis.group.map(d => ({
-            ...d,
-            radius: vis.size(d.value),
-            x: Math.random() * 900,
-            y: Math.random() * 800
-        }))
-
-        vis.renderVis();
-    }
-
-
-    renderVis() {
-        let vis = this;
-
-        const bubbles = vis.chart.selectAll(".bubble")
-            .data(vis.nodes)
-            .join(
-                enter => enter.append("circle").attr("class", "bubble"),
-                update => update,
-                exit => exit.remove()
-            )
-            .attr('r', d => d.radius)
-            .attr('fill', d => vis.color(d.type))
-            .on("mouseover", (event, d) => {
-                d3.select('#tooltip')
-                    .style('display', 'block')
-                    .style("left", (event.pageX + 20) + "px")
-                    .style("top", (event.pageY) + "px")
-                    .html(`
+    const bubbles = vis.chart
+      .selectAll(".bubble")
+      .data(vis.nodes)
+      .join(
+        (enter) => enter.append("circle").attr("class", "bubble"),
+        (update) => update,
+        (exit) => exit.remove()
+      )
+      .attr("r", (d) => d.radius)
+      .attr("fill", (d) => vis.color(d.type))
+      .on("mouseover", (event, d) => {
+        d3
+          .select("#tooltip")
+          .style("display", "block")
+          .style("left", event.pageX + 20 + "px")
+          .style("top", event.pageY + "px").html(`
             <div class='tooltip-title'>${d.type}: ${d.breed}</div>
               <div> ${d.value} </div>
           `);
-            })
-            .on("mouseleave", () => {
-                d3.select('#tooltip').style('display', 'none');
-            })
-            .call(d3.drag() // call specific function when circle is dragged
-                .on("start", dragstarted)
-                .on("drag", dragged)
-                .on("end", dragended));
+      })
+      .on("mouseleave", () => {
+        d3.select("#tooltip").style("display", "none");
+      })
+      .call(
+        d3
+          .drag() // call specific function when circle is dragged
+          .on("start", dragstarted)
+          .on("drag", dragged)
+          .on("end", dragended)
+      );
 
-        function dragstarted(event, d) {
-            if (!event.active) vis.simulation.alphaTarget(.03).restart();
-            d.fx = d.x;
-            d.fy = d.y;
-        }
-        function dragged(event, d) {
-            d.fx = event.x;
-            d.fy = event.y;
-        }
-        function dragended(event, d) {
-            if (!event.active) vis.simulation.alphaTarget(.03);
-            d.fx = null;
-            d.fy = null;
-        }
-
-        const label = vis.chart.selectAll(".label")
-            .data(vis.nodes)
-            .join(
-                enter => enter.append("text").attr("class", "label")
-                    .attr('dy', '.3em')
-                    .style('text-anchor', 'middle')
-                    .style('font-size', 12),
-                update => update,
-                exit => exit.remove()
-            )
-            .text(function (d) {
-                return ((d.value / vis.data.length) * 100 > 1) ? ((d.value / vis.data.length) * 100).toFixed(2) + "%" : '';
-            })
-
-        const label1 = vis.chart.selectAll(".label1")
-            .data(vis.nodes)
-            .join(
-                enter => enter.append("text").attr("class", "label1")
-                    .attr('dy', '2em')
-                    .style('text-anchor', 'middle')
-                    .style('font-size', 7),
-                update => update,
-                exit => exit.remove()
-            )
-            .text(function (d) {
-                return ((d.value / vis.data.length) * 100 > 1) ? d.breed : '';
-            })
-
-        vis.simulation
-            .nodes(vis.nodes)
-            .on("tick", function (d) {
-                bubbles
-                    .attr("cx", d => d.x)
-                    .attr("cy", d => d.y)
-                label
-                    .attr("x", d => d.x)
-                    .attr("y", d => d.y)
-                label1
-                    .attr("x", d => d.x)
-                    .attr("y", d => d.y)
-            })
-            .restart();
-
-
+    function dragstarted(event, d) {
+      if (!event.active) vis.simulation.alphaTarget(0.03).restart();
+      d.fx = d.x;
+      d.fy = d.y;
     }
-}
+    function dragged(event, d) {
+      d.fx = event.x;
+      d.fy = event.y;
+    }
+    function dragended(event, d) {
+      if (!event.active) vis.simulation.alphaTarget(0.03);
+      d.fx = null;
+      d.fy = null;
+    }
 
+    const label = vis.chart
+      .selectAll(".label")
+      .data(vis.nodes)
+      .join(
+        (enter) =>
+          enter
+            .append("text")
+            .attr("class", "label")
+            .attr("dy", ".3em")
+            .style("text-anchor", "middle")
+            .style("font-size", 12),
+        (update) => update,
+        (exit) => exit.remove()
+      )
+      .text(function (d) {
+        return (d.value / vis.data.length) * 100 > 1
+          ? ((d.value / vis.data.length) * 100).toFixed(2) + "%"
+          : "";
+      });
+
+    const label1 = vis.chart
+      .selectAll(".label1")
+      .data(vis.nodes)
+      .join(
+        (enter) =>
+          enter
+            .append("text")
+            .attr("class", "label1")
+            .attr("dy", "2em")
+            .style("text-anchor", "middle")
+            .style("font-size", 7),
+        (update) => update,
+        (exit) => exit.remove()
+      )
+      .text(function (d) {
+        return (d.value / vis.data.length) * 100 > 1 ? d.breed : "";
+      });
+
+    vis.simulation
+      .nodes(vis.nodes)
+      .on("tick", function (d) {
+        bubbles.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+        label.attr("x", (d) => d.x).attr("y", (d) => d.y);
+        label1.attr("x", (d) => d.x).attr("y", (d) => d.y);
+      })
+      .restart();
+  }
+}
